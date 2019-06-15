d92a9bc40e98cf70057f5f61b514b0c1cd805214
cassa10 <cassanojoseluis@gmail.com>
2019-06-15 20:48:27 -0300

    Agrego un cambio de color cuando se visualiza la tarea y manejo los inputs cuando se agrega una tarea.

--- frontend/src/components/Materia.jsx
+++ frontend/src/components/Materia.jsx
@@ -14,6 +14,8 @@ class Materia extends React.Component {
         fechaEntrega: '',
         nombreTarea: '',
       },
+      errorModalTituloTarea: '',
+      errorModalFechaTarea: '',
     };
   }
 
@@ -23,9 +25,49 @@ class Materia extends React.Component {
       .catch(error => console.log(error));
   }
 
+  setErrorCorrepondiente() {
+    if (this.state.tarea.nombreTarea === '') {
+      this.setState({ errorModalTituloTarea: 'Tarea sin nombre' });
+    }
+    if (this.state.tarea.fechaEntrega === '') {
+      this.setState({ errorModalFechaTarea: 'Fecha invalida' });
+    }
+  }
+
+  getDiaActual() {
+    const hoy = new Date();
+    const dia = this.parsePositiveIntTwoDigits(hoy.getDate());
+    const mes = this.parsePositiveIntTwoDigits(hoy.getMonth() + 1);
+
+    return `${hoy.getFullYear()}-${mes}-${dia}`;
+  }
+
+  crearTextoDeDiasRestantes(daysLeft) {
+    if (daysLeft >= 0) {
+      if (daysLeft > 0) {
+        return `${daysLeft} día(s) restantes`;
+      }
+      return 'Hoy se entrega';
+    }
+    return 'Tarea finalizada';
+  }
+
+  crearColorSegunFecha(daysLeft) {
+    if (daysLeft >= 0) {
+      if (daysLeft > 0) {
+        return 'alert alert-info col-6';
+      }
+      return 'alert alert-warning col-6';
+    }
+    return 'alert alert-danger col-6';
+  }
+
   crearBodyTarea(tarea) {
+    const daysLeft = Math.round((new Date(tarea.fecha)
+      .getTime() - new Date().getTime()) / 86400000) + 1;
+
     return (
-      <div className="alert alert-info col-6" role="alert">
+      <div className={this.crearColorSegunFecha(daysLeft)} role="alert">
         <div className="row">
           <div className="col-3">
             {this.formato(tarea.fecha)}
@@ -33,10 +75,8 @@ class Materia extends React.Component {
           <div className="col-3">
             {tarea.nombre}
           </div>
-          <div className="col-4">
-            {Math
-              .round((new Date(tarea.fecha)
-                .getTime() - new Date().getTime()) / 86400000) + 1} dias restantes
+          <div>
+            {this.crearTextoDeDiasRestantes(daysLeft)}
           </div>
         </div>
       </div>
@@ -73,26 +113,61 @@ class Materia extends React.Component {
 
   handlerFecha(event) {
     const oldTarea = this.state.tarea;
-    this.setState({ tarea: { ...oldTarea, fechaEntrega: this.formato(event.target.value) } });
+    this.setState({ errorModalFechaTarea: '', tarea: { ...oldTarea, fechaEntrega: this.formato(event.target.value) } });
   }
 
   handlerTituloTarea(event) {
     const oldTarea = this.state.tarea;
-    this.setState({ tarea: { ...oldTarea, nombreTarea: event.target.value } });
+    this.setState({ errorModalTituloTarea: '', tarea: { ...oldTarea, nombreTarea: event.target.value } });
   }
 
   crearTareaNueva() {
-    API.post(`/tarea/${this.state.materia.id}`, this.state.tarea)
-      .then(response => this.setState({ materia: response }), this.cancelarTarea())
-      .catch(error => console.log(error.response));
+    if (this.state.tarea.nombreTarea === '' || this.state.tarea.fechaEntrega === '') {
+      this.setErrorCorrepondiente();
+    } else {
+      API.post(`/tarea/${this.state.materia.id}`, this.state.tarea)
+        .then(response => this.setState({ materia: response }), this.cancelarTarea())
+        .catch(error => console.log(error.response));
+    }
   }
 
   cancelarTarea() {
-    this.setState({ tarea: { nombreTarea: '', fechaEntrega: '' } });
+    this.setState({ errorModalFechaTarea: '', errorModalTituloTarea: '', tarea: { nombreTarea: '', fechaEntrega: '' } });
   }
 
   formato(texto) {
     return texto.replace(/^(\d{4})-(\d{2})-(\d{2})$/g, '$3/$2/$1');
+  }
+
+  handleSiExisteErrorEnTitulo() {
+    if (this.state.errorModalTituloTarea !== '') {
+      return (
+        <div className="invalid">
+          <span className="oi oi-x" />
+          {this.state.errorModalTituloTarea}
+        </div>
+      );
+    }
+    return undefined;
+  }
+
+  handleSiExisteErrorEnFecha() {
+    if (this.state.errorModalFechaTarea !== '') {
+      return (
+        <div className="invalid">
+          <span className="oi oi-x" />
+          {this.state.errorModalFechaTarea}
+        </div>
+      );
+    }
+    return undefined;
+  }
+
+  parsePositiveIntTwoDigits(n) {
+    if (n < 10) {
+      return `0${n}`;
+    }
+    return n;
   }
 
   render() {
@@ -116,10 +191,12 @@ class Materia extends React.Component {
                     <label htmlFor="exampleInputEmail1">Titulo</label>
                     <input type="text" className="form-control" placeholder="..." value={this.state.tarea.nombreTarea} onChange={event => this.handlerTituloTarea(event)} />
                   </div>
+                  {this.handleSiExisteErrorEnTitulo()}
                   <div className="form-group">
                     <label htmlFor="fecha1">Fecha</label>
-                    <input type="date" className="form-control" id="fecha1" min="2019-01-01" pattern="[0-9]{4}/[0-9]{2}/[0-9]{2}" onChange={event => this.handlerFecha(event)} />
+                    <input type="date" className="form-control" id="fecha1" min={this.getDiaActual()} pattern="[0-9]{4}/[0-9]{2}/[0-9]{2}" onChange={event => this.handlerFecha(event)} />
                   </div>
+                  {this.handleSiExisteErrorEnFecha()}
                 </form>
               </div>
               <div className="modal-footer">
